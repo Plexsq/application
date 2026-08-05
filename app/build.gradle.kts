c79f85089bc4ba529b9e59e0e182257184646044
Plexsq <plexsq@users.noreply.github.com>
2026-08-05 06:37:35 +0300

    Apply Kotlin compose compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.compose)
 }
 
 val keystoreProps = Properties().apply {
