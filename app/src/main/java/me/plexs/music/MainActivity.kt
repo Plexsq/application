ebae91105074feb1e0b9f782c4ce1b1f16192e26
Plexsq <plexsq@users.noreply.github.com>
2026-08-05 18:17:47 +0300

    Fix compile errors: add material-icons-extended and Modifier import

--- app/src/main/java/me/plexs/music/MainActivity.kt
+++ app/src/main/java/me/plexs/music/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
